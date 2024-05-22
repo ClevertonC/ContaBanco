db253f46c2257bc3b818a052755269dc6d807655
Cleverton Costa <86128790+ClevertonC@users.noreply.github.com>
2024-05-21 21:41:00 -0300

    Add files via upload

---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,31 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+
+public class ContaTerminal {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        //crias as vairaveis como nome,agencia,saldo pra receber os valores  e cria um if e else caso n
+        //coloca as informação devidas e então poder sair do programa
+        //fazer uma solicitação de saldo
+        /*solicita um double de 25 no saldo*/
+        String info = "(depois ENTER para o próximo campo)";
+        System.out.println("Por favor, digite o número da Agência !");
+        System.out.println(" usuário : " + info);
+        int numero = scanner.nextInt();
+
+        System.out.println(" nome : " + info);
+        String nomeCliente = scanner.next();
+        System.out.println(" agencia : "+ info );
+        String agencia = scanner.next();
+        System.out.println(" saldo : "+ info);
+        double saldo= scanner.nextDouble();
+
+
+        /*imprimi usando concatenar
+         * "Olá [Nome Cliente], obrigado por criar uma conta em nosso banco, sua agência é [Agencia]
+         * , conta [Numero] e seu saldo [Saldo] já está disponível para saque".*/
+
+        System.out.println("Olá " + nomeCliente + " obrigado por criar uma conta em nosso banco, sua agência é "+ agencia +", conta " + numero + "e seu saldo "+ saldo + " já está disponível para saque ");
+    }
+}
